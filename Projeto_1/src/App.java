6df0986f098dfb7a8042c7f67321279a6fa3872e
Artur Sousa <arturavs1996@gmail.com>
2023-11-02 17:56:47 -0300

    Limpeza da classe Main

--- Projeto_1/src/App.java
+++ Projeto_1/src/App.java
@@ -1,14 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Inicio da Atividade");
-        Medico m1 = new Medico();
-        m1.nome_medico = "Artur Vinicius Araujo Vieira de Sousa";
-        m1.num_crm = 9533;
-        m1.nome_UF_crm = "CE";
-        m1.num_cpf = "026054163035";
-        m1.num_rg = "2009009132966";  
-        m1.endereco_email = "arturavs1996@gmail.com";
-        m1.num_contato = "85-997376830";
-        m1.exibe_dados();
+        
+        System.out.print("Inicio da Atividade");
     }
 }
--- Projeto_1/src/Medico.java
+++ Projeto_1/src/Medico.java
@@ -1,14 +1,81 @@
 public class Medico {
-    String nome_medico;
-    int num_crm;
-    String nome_UF_crm;
-    String num_cpf;
-    String num_rg;
-    String endereco_email;
-    String num_contato;
+    public String nome_medico;
+    public int num_crm;
+    public String nome_UF_crm;
+    private String num_cpf;
+    private String num_rg;
+    public String endereco_email;
+    public String num_contato;
+    public String nome_especialidade;
 
-    public void cadastro_medico(){
-        
+    public String get_Nome(){
+        return this.nome_medico;
+    };
+
+    public int get_Crm(){
+        return this.num_crm;
+    };
+
+    public String get_UF(){
+        return this.nome_UF_crm;
+    };
+
+    public String get_Cpf(){
+        return this.num_cpf;
+    };
+
+    public String get_Rg(){
+        return this.num_rg;
+    };
+
+    public String get_Email(){
+        return this.endereco_email;
+    };
+
+    public String get_Contato(){
+        return this.num_contato;
+    };
+
+    public String get_Especialidade(){
+        return this.nome_especialidade;
+    };
+      
+    
+    public void set_Nome(String nome){
+        this.nome_medico = nome;
+    };
+
+    public void set_Crm(int crm){
+        this.num_crm = crm;
+    };
+
+    public void set_UF(String uf){
+        this.nome_UF_crm = uf;
+    };
+
+    public void set_Cpf(String cpf){
+        this.num_cpf = cpf;
+    };
+
+    public void set_RG(String rg){
+        this.num_rg = rg;
+    };
+
+    public void set_Email(String email){
+        this.endereco_email = email;
+    };
+
+    public void set_Contato(String contato){
+        this.num_contato = contato;
+    }
+
+    public void set_Especialidade(String especialidade){
+        this.nome_especialidade = especialidade;
+    }
+
+    
+
+    public void cadastro_medico(String nome, int crm, String UF, String cpf, String rg, String email, String contato){
 
 
 
