408933b48d0ae63e560c1064db3cb23a3e5966ba
Artur <arturavs1996@gmail.com>
2023-11-03 19:16:34 -0300

    Testando os métodos getters e setters no Main

--- Projeto_1/src/App.java
+++ Projeto_1/src/App.java
@@ -2,5 +2,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         System.out.print("Inicio da Atividade");
+        Medico m1 = new Medico();
+        m1.set_Nome("Artur Vinicius Araujo");
+        m1.set_Crm(9533);
+        m1.set_Contato("85997376830");
+        m1.set_Cpf("02605416305");
+        m1.set_RG("2009009132966");
+        m1.set_Email("arturavs1996@gmail.com");
+        m1.set_Especialidade("Clínica Médica");
+        m1.set_UF("CE");
+        
+        m1.exibe_dados();
+
     }
 }
