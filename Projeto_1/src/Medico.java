8928db105eb8401c7aef771fbd740704dbd317a2
Artur Sousa <arturavs1996@gmail.com>
2023-11-22 17:03:11 -0300

    Corrigido o método construtor, o método 'exibe dados'. Instanciado um novo objeto com o métoo construtor e validando o método exibe_dados();'

--- Projeto_1/src/Medico.java
+++ Projeto_1/src/Medico.java
@@ -1,93 +1,94 @@
 public class Medico {
-    public String nome_medico;
-    public int num_crm;
-    public String nome_UF_crm;
-    private String num_cpf;
-    private String num_rg;
-    public String endereco_email;
-    public String num_contato;
-    public String nome_especialidade;
+    public String nomeMedico;
+    public int numCrm;
+    public String nomeUfCrm;
+    private String numCpf;
+    private String numRg;
+    public String enderecoEmail;
+    public String numContato;
+    public String nomeEspecialidade;
 
-    public String get_Nome(){
-        return this.nome_medico;
-    };
-
-    public int get_Crm(){
-        return this.num_crm;
-    };
-
-    public String get_UF(){
-        return this.nome_UF_crm;
-    };
-
-    public String get_Cpf(){
-        return this.num_cpf;
-    };
-
-    public String get_Rg(){
-        return this.num_rg;
-    };
-
-    public String get_Email(){
-        return this.endereco_email;
-    };
-
-    public String get_Contato(){
-        return this.num_contato;
-    };
-
-    public String get_Especialidade(){
-        return this.nome_especialidade;
-    };
-      
-    
-    public void set_Nome(String nome){
-        this.nome_medico = nome;
-    };
-
-    public void set_Crm(int crm){
-        this.num_crm = crm;
-    };
-
-    public void set_UF(String uf){
-        this.nome_UF_crm = uf;
-    };
-
-    public void set_Cpf(String cpf){
-        this.num_cpf = cpf;
-    };
-
-    public void set_RG(String rg){
-        this.num_rg = rg;
-    };
-
-    public void set_Email(String email){
-        this.endereco_email = email;
-    };
-
-    public void set_Contato(String contato){
-        this.num_contato = contato;
+    public String getNome() {
+        return this.nomeMedico;
     }
 
-    public void set_Especialidade(String especialidade){
-        this.nome_especialidade = especialidade;
+    public int getCrm() {
+        return this.numCrm;
     }
 
-    
-
-    public void cadastro_medico(String nome, int crm, String UF, String cpf, String rg, String email, String contato){
-
-
-
+    public String getUf() {
+        return this.nomeUfCrm;
     }
 
-    public void exibe_dados(){
-        System.out.println("Nome: "+ this.nome_medico);
-        System.out.println("CRM: "+ this.num_crm);
-        System.out.println("UF: "+ this.nome_UF_crm);
-        System.out.println("CPF: "+ this.num_cpf);
-        System.out.println("RG: "+ this.num_rg);
-        System.out.println("RG: "+ this.endereco_email);
-        System.out.println("RG: "+ this.num_contato);
+    public String getCpf() {
+        return this.numCpf;
+    }
+
+    public String getRg() {
+        return this.numRg;
+    }
+
+    public String getEmail() {
+        return this.enderecoEmail;
+    }
+
+    public String getContato() {
+        return this.numContato;
+    }
+
+    public String getEspecialidade() {
+        return this.nomeEspecialidade;
+    }
+
+    public void setNome(String nome) {
+        this.nomeMedico = nome;
+    }
+
+    public void setCrm(int crm) {
+        this.numCrm = crm;
+    }
+
+    public void setUf(String uf) {
+        this.nomeUfCrm = uf;
+    }
+
+    public void setCpf(String cpf) {
+        this.numCpf = cpf;
+    }
+
+    public void setRg(String rg) {
+        this.numRg = rg;
+    }
+
+    public void setEmail(String email) {
+        this.enderecoEmail = email;
+    }
+
+    public void setContato(String contato) {
+        this.numContato = contato;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.nomeEspecialidade = especialidade;
+    }
+
+    public Medico(String nome, int crm, String uf, String cpf, String rg, String email, String contato) {
+        this.nomeMedico = nome;
+        this.numCrm = crm;
+        this.nomeUfCrm = uf;
+        this.numCpf = cpf;
+        this.numRg = rg;
+        this.enderecoEmail = email;
+        this.numContato = contato;
+    }
+
+    public void exibeDados() {
+        System.out.println("Nome: " + this.nomeMedico);
+        System.out.println("CRM: " + this.numCrm);
+        System.out.println("UF: " + this.nomeUfCrm);
+        System.out.println("CPF: " + this.numCpf);
+        System.out.println("RG: " + this.numRg);
+        System.out.println("E-mail: " + this.enderecoEmail);
+        System.out.println("Contato: " + this.numContato);
     }
 }
